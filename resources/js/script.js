5e14a3425b10ab46c88e5231b426aefcb0862d51
“Sam <sam.alsmadi@gmail.com>
2018-05-13 00:06:55 -0400

    Add a sticky nav bar

---
+++ resources/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  // test connection!
+  // $('h1').click(function() {
+  //   console.log('Clicked!');
+  //   $(this).css('background-color', 'red');
+  // })
+
+
+
+})
+
+// var h1Elements = document.getElementsByTagName('h1');
+// for (var i = 0; i < h1Elements.length; i++) {
+//   h1Elements[i].style.backgroundColor='red';
+// }
