bede69322869a781ef349221bf866628ccd92901
“Sam <sam.alsmadi@gmail.com>
2018-05-13 15:39:57 -0400

    Add scroll animations with Animate.css

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     offset: '60px'
   });
 
-    /* Scroll on buttons */
+  /* Scroll on buttons */
   $('.js--scroll-to-plans').click(function() {
     $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
   });
@@ -20,23 +20,7 @@ $(document).ready(function(){
   });
 
   /* Navigation scroll */
-
-  // $(function () {
-  //   $('a[href*=#]:not([href=#])').click(function () {
-  //     if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-  //       var target = $(this.hash);
-  //       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-  //       if (target.length) {
-  //         $('html,body').animate({
-  //           scrollTop: target.offset().top
-  //         }, 1000);
-  //         return false;
-  //       }
-  //     }
-  //   });
-  // });
-
-  $("a").on('click', function (event) {
+  $('a').on('click', function (event) {
     if (this.hash !== '') {
       event.preventDefault();
       var hash = this.hash;
@@ -48,5 +32,30 @@ $(document).ready(function(){
       });
     }
   });
-  
+
+  /* Animations on scroll */
+  $('.js--wp-1').waypoint(function (direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+      offset: '50%'
+    });
+
+  $('.js--wp-2').waypoint(function (direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+      offset: '50%'
+    });
+
+  $('.js--wp-3').waypoint(function (direction) {
+    $('.js--wp-3').addClass('animated fadeIn');
+  }, {
+      offset: '50%'
+    });
+
+  $('.js--wp-4').waypoint(function (direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+      offset: '50%'
+    });
+
 });
